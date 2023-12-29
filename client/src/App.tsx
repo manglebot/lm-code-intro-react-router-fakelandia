1685be8f3e45f5ba2c6746b48595e0c5e5d67a7a
Ben <manglebot@users.noreply.github.com>
2023-12-29 16:43:59 +0000

    feat:added pages to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import Confession from "./components/Confession";
+import Misdemeanour from "./components/Misdemeanour";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/confession" element={<Confession />} />
+        <Route path="/misdemeanour" element={<Misdemeanour />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
