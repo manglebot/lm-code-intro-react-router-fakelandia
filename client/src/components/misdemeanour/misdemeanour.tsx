5f4367c000e311818b1331773115856ccc853540
Ben <manglebot@users.noreply.github.com>
2023-12-29 22:19:22 +0000

    feat:update misdemeanour with tabular data

--- client/src/components/misdemeanour/misdemeanour.tsx
+++ client/src/components/misdemeanour/misdemeanour.tsx
@@ -1,7 +1,52 @@
+import { useEffect, useState } from "react";
+
+type MisdemeanourProps = {
+  citizenId: number;
+  date: string;
+  misdemeanour: string;
+};
+
 const Misdemeanour: React.FC = () => {
+  const [misdemeanours, setMisdemeanours] = useState<MisdemeanourProps[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/api/misdemeanours/5"
+        );
+        const data = await response.json();
+        setMisdemeanours(data.misdemeanours);
+      } catch (error) {
+        // error code to follow
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>Misdemeanour!</h1>
+      <table className="misdemeanour__table">
+        <thead>
+          <tr>
+            <th>Citizen ID</th>
+            <th>Date</th>
+            <th>Misdemeanour</th>
+            <th>Punishment Idea</th>
+          </tr>
+        </thead>
+        <tbody>
+          {misdemeanours.map((misdemeanour, index) => (
+            <tr key={index}>
+              <td>{misdemeanour.citizenId}</td>
+              <td>{misdemeanour.date}</td>
+              <td>{misdemeanour.misdemeanour}</td>
+              <td>data to follow</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
