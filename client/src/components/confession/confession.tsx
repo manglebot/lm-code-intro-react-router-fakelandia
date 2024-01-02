80b716dadcbd20f0b47cc4c26e7918d8c71d139a
Ben <manglebot@users.noreply.github.com>
2024-01-02 11:25:07 +0000

    feat:set up confession page

--- client/src/components/confession/confession.tsx
+++ client/src/components/confession/confession.tsx
@@ -1,7 +1,64 @@
 const Confession: React.FC = () => {
   return (
     <div>
-      <h1>Confession!</h1>
+      <main>
+        <p>
+          It's very difficult to catch people committing misdemeanours so we
+          appreciate it when citizens confess to us directly.
+        </p>
+        <p>
+          However, if you're just having a hard day and need to vent then you're
+          welcome to contact us here too. Up to you!
+        </p>
+        <form action="/submit" method="post" className="confess-form">
+          <label htmlFor="subject" className="confess-form__label">
+            Subject:
+          </label>
+          <br />
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            className="confess-form__input"
+          />
+          <br />
+          <br />
+
+          <label htmlFor="reason" className="confess-form__label">
+            Reason for Contact:
+          </label>
+          <br />
+          <select id="reason" name="reason" className="confess-form__select">
+            <option value="misdemeanour">Misdemeanour</option>
+            <option value="talk">I just want to talk</option>
+          </select>
+          <br />
+          <br />
+
+          <label htmlFor="message" className="confess-form__label">
+            Message:
+          </label>
+          <br />
+          <textarea
+            id="message"
+            name="message"
+            style={{ width: "100%", resize: "vertical" }}
+            rows={4}
+            cols={50}
+            className="confess-form__textarea"
+          ></textarea>
+
+          <br />
+          <br />
+
+          <input
+            type="submit"
+            value="Confess"
+            className="confess-form__submit"
+            disabled
+          />
+        </form>
+      </main>
     </div>
   );
 };
