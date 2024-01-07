40de710d2a17d15ff320b93f8c443ce5708e689a
Ben <manglebot@users.noreply.github.com>
2024-01-07 21:56:24 +0000

    test:add test for misdemeanour api call

--- client/src/components/misdemeanour/misdemeanour.test.tsx
+++ client/src/components/misdemeanour/misdemeanour.test.tsx
@@ -1,9 +1,38 @@
 import { render } from "@testing-library/react";
 import Misdemeanour from "./misdemeanour";
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  test,
+  it,
+  vi,
+  JestAssertion,
+} from "vitest";
+import { ViteDevServer } from "vite";
 
 // import { render, waitFor, screen } from "@testing-library/react";
 
-//just check rendering
-test("renders DailyMix component without crashing", () => {
-  render(<Misdemeanour />);
+describe("Tests for Misdemeanour page", () => {
+  test("renders Misdemeanour component without crashing", () => {
+    // Arrange & Act
+    const { getByText } = render(<Misdemeanour />);
+    // Assert
+    expect(getByText("Citizen ID")).toBeInTheDocument();
+  });
+});
+
+describe("Tests for API Call", () => {
+  global.fetch = vi.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({}),
+    })
+  );
+  test("API call is made on component mount", () => {
+    render(<Misdemeanour />);
+    expect(fetch).toHaveBeenCalledWith(
+      "http://localhost:8080/api/misdemeanours/10"
+    );
+  });
 });
