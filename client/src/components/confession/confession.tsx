cafda8ab8442bde6136298b106063be9a0af8f26
Ben <manglebot@users.noreply.github.com>
2024-01-07 14:45:31 +0000

    feat:add form validation

--- client/src/components/confession/confession.tsx
+++ client/src/components/confession/confession.tsx
@@ -1,4 +1,44 @@
+import { useState } from "react";
+import { Url, UrlObject } from "url";
+
 const Confession: React.FC = () => {
+  const [formData, setFormData] = useState({
+    subject: "",
+    message: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    if (value.trim() === "") {
+      setErrorMessage("Please fill out all required fields.");
+    } else {
+      handleInputChange;
+      setErrorMessage("");
+    }
+  };
+
+  const handleSubmit = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    e.preventDefault();
+    if (formData.subject === "" || formData.message === "") {
+      setErrorMessage("Please fill out all required fields.");
+      return;
+    }
+    setErrorMessage("");
+  };
+
+  const isFormValid = formData.subject !== "" && formData.message !== "";
+
   return (
     <div>
       <main>
@@ -10,54 +50,76 @@ const Confession: React.FC = () => {
           However, if you're just having a hard day and need to vent then you're
           welcome to contact us here too. Up to you!
         </p>
-        <form action="/submit" method="post" className="confess-form">
-          <label htmlFor="subject" className="confess-form__label">
-            Subject:
-          </label>
-          <br />
-          <input
-            type="text"
-            id="subject"
-            name="subject"
-            className="confess-form__input"
-          />
-          <br />
-          <br />
+        <div className="confess-form__container">
+          <form
+            action="/submit"
+            method="post"
+            className="confess-form"
+            onSubmit={handleSubmit}
+          >
+            <label htmlFor="subject" className="confess-form__label-subject">
+              Subject:
+            </label>
+            <input
+              type="text"
+              id="subject"
+              name="subject"
+              className={`confess-form__input-subject ${
+                formData.subject === ""
+                  ? "confess-form__input-subject--invalid"
+                  : ""
+              }`}
+              onChange={handleInputChange}
+            />
+            <br />
+            <label htmlFor="reason" className="confess-form__label-reason">
+              Reason for Contact:
+            </label>
+            <br />
+            <select
+              id="reason"
+              name="reason"
+              className="confess-form__select-reason"
+            >
+              <option value="misdemeanour">Misdemeanour</option>
+              <option value="talk">I just want to talk</option>
+            </select>
+            <br />
+            <textarea
+              id="message"
+              name="message"
+              style={{ width: "100%", resize: "vertical" }}
+              rows={4}
+              cols={50}
+              className={`confess-form__textarea ${
+                formData.message === "" ? "confess-form__textarea--invalid" : ""
+              }`}
+              onChange={handleInputChange}
+            ></textarea>
 
-          <label htmlFor="reason" className="confess-form__label">
-            Reason for Contact:
-          </label>
-          <br />
-          <select id="reason" name="reason" className="confess-form__select">
-            <option value="misdemeanour">Misdemeanour</option>
-            <option value="talk">I just want to talk</option>
-          </select>
-          <br />
-          <br />
+            <br />
+            <br />
 
-          <label htmlFor="message" className="confess-form__label">
-            Message:
-          </label>
-          <br />
-          <textarea
-            id="message"
-            name="message"
-            style={{ width: "100%", resize: "vertical" }}
-            rows={4}
-            cols={50}
-            className="confess-form__textarea"
-          ></textarea>
-
-          <br />
-          <br />
-
-          <input
-            type="submit"
-            value="Confess"
-            className="confess-form__submit"
-            disabled
-          />
-        </form>
+            <input
+              type="submit"
+              value="Confess"
+              className={`confess-form__submit ${
+                formData.message === "" ? "confess-form__submit--disabled" : ""
+              }`}
+            />
+            {errorMessage && (
+              <p
+                className={`confess-form__error-message ${
+                  formData.subject === "" || formData.message === ""
+                    ? "confess-form__error-message--show"
+                    : ""
+                }`}
+              >
+                {errorMessage}
+              </p>
+            )}
+          </form>
+        </div>
       </main>
     </div>
   );
