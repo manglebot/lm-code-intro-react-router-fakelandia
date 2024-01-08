92aeacf4ddaac662a6b03b6ff55d01f24c6f3b3c
Ben <manglebot@users.noreply.github.com>
2024-01-08 11:01:35 +0000

    feat:add homepage text

--- client/src/components/home/home.tsx
+++ client/src/components/home/home.tsx
@@ -1,7 +1,10 @@
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Home!</h1>
+      <h1 className="home_title">Welcome to Fakelandia's Justice Department</h1>
+      <h2 className="home_subtitle">
+        Discover Recent Misdemeanours or Confess Your Own
+      </h2>
     </div>
   );
 };
