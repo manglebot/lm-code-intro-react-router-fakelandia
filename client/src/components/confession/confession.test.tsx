0e9cedb24871f61ff24f181c2cf7b629e43e4806
Ben <manglebot@users.noreply.github.com>
2024-01-08 11:41:20 +0000

    test:add tests for confession

--- client/src/components/confession/confession.test.tsx
+++ client/src/components/confession/confession.test.tsx
@@ -1,7 +1,33 @@
-import { render, waitFor, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Confession from "./confession";
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  test,
+  it,
+  vi,
+  JestAssertion,
+} from "vitest";
+import { ViteDevServer } from "vite";
 
-//just check rendering
-test("renders DailyMix component without crashing", () => {
-  render(<Confession />);
+describe("Tests for Confession page", () => {
+  test("renders Confession component without crashing", () => {
+    // Arrange & Act
+    const { container } = render(<Confession />);
+    // Assert
+    container.querySelector(".confession__description");
+  });
+
+  // test("renders text on Confession page", () => {
+  //   // Arrange & Act
+  //   const { getByText, getByLabelText } = render(<Confession />);h
+
+  //   // Assert
+  //   expect(
+  //     getByText("It's very difficult to catch people committing misdemeanours")
+  //   ).toBeInTheDocument();
+  //   expect(getByLabelText("Subject:")).toBeInTheDocument();
+  // });
 });
